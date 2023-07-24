4a19fc12145018b609696bd959696b5dea8df84d
Pof Magicfingers <pof@podcloud.fr>
2023-07-24 19:37:32 +0200

    feat: add new color

--- scripts/generate-themes.ts
+++ scripts/generate-themes.ts
@@ -2,10 +2,13 @@ import chroma from 'chroma-js'
 import type { ThemeColors } from '~/composables/settings'
 
 // #cc7d24 -> hcl(67.14,62.19,59.56)
-export const themesColor = Array.from(
-  { length: 9 },
-  (_, i) => chroma.hcl((67.14 + i * 40) % 360, 62.19, 59.56).hex(),
-)
+export const themesColor = [
+  '#b82828',
+  ...Array.from(
+    { length: 9 },
+    (_, i) => chroma.hcl((67.14 + i * 40) % 360, 62.19, 59.56).hex(),
+  ),
+]
 
 export function getThemeColors(primary: string): ThemeColors {
   const c = chroma(primary)
